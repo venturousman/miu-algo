a82a743398b867bd8551ff58f9d715b35e30a2ae
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2025-01-14 21:04:37 -0600

    commit solution for question 1

---
+++ w1d2/src/question1/Solution.java
@@ -0,0 +1,109 @@
+package question1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Solution {
+
+	private static int findThirdLargest1(int[] a) {
+		/*
+		 * Idea – Use three loops one after another. First loop will find Max. Second
+		 * loop will find Second Max, Third loop will find third max. Note that it is
+		 * possible First max == second Max == Third Max as in 7, 20, 18, 4, 20, 19, 20,
+		 * 3. and your program should return 20 in this case.
+		 */
+		if (a == null || a.length < 3) {
+			throw new IllegalArgumentException("Array must contain at least three elements.");
+		}
+
+		int n = a.length;
+		int index = 0;
+		for (int i = 1; i < n; i++) {
+			if (a[i] > a[index]) {
+				index = i;
+			}
+		}
+		int secondIndex = 0;
+		for (int i = 1; i < n; i++) {
+			if (a[i] > a[secondIndex] && i != index) {
+				secondIndex = i;
+			}
+		}
+		int thirdIndex = 0;
+		for (int i = 1; i < n; i++) {
+			if (a[i] > a[thirdIndex] && i != index && i != secondIndex) {
+				thirdIndex = i;
+			}
+		}
+//		System.out.println(index + ", " + secondIndex + ", " + thirdIndex);
+		return a[thirdIndex];
+	}
+
+	public static int findThirdLargest2(int[] nums) {
+		/*
+		 * Idea – Use one loop. Maintain three variable max, preMax and prePreMax such
+		 * that max will have the maximum value, preMax will have the second largest and
+		 * prePreMax will have the third largest value.
+		 */
+		if (nums == null || nums.length < 3) {
+			throw new IllegalArgumentException("Array must contain at least three elements.");
+		}
+
+		Integer first = null, second = null, third = null;
+
+		for (int num : nums) {
+			if (first == null || num > first) {
+				third = second;
+				second = first;
+				first = num;
+			} else if (second == null || (num > second && num <= first)) {
+				third = second;
+				second = num;
+			} else if (third == null || (num > third && num <= second)) {
+				third = num;
+			}
+		}
+
+		if (third == null) {
+			throw new IllegalArgumentException("Array must have at least three unique numbers.");
+		}
+
+		return third;
+	}
+
+	public static int findThirdLargest3(int[] nums) {
+		// Idea – Use an ordered dictionary.
+		if (nums == null || nums.length < 3) {
+			throw new IllegalArgumentException("Array must contain at least three elements.");
+		}
+//		LinkedHashMap<Integer, Integer> lhm = new LinkedHashMap<>();
+////		for (int num : nums) {
+////			lhm.put(num, num);
+////		}
+//		for (int i = 0; i < nums.length; i++) {
+//			lhm.put(i, nums[i]);
+//		}
+//
+//		// Displaying the LinkedHashMap
+//		System.out.println("" + lhm);
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 7, 20, 18, 4, 20, 19, 20, 3 };
+//		int[] arr = { 7, 20, 18, 4, 21, 19, 5, 3 };
+//		int[] arr = { 5, 1, 2, 9, 7, 7, 3 };
+//		int[] arr = { 5, 1, 2, 6, 7, 7, 3 };
+//        int[] arr = {10, 20, 4, 45, 99, 87, 76};
+
+		System.out.println(findThirdLargest1(arr));
+		System.out.println(findThirdLargest2(arr));
+		System.out.println(findThirdLargest3(arr));
+	}
+
+}
